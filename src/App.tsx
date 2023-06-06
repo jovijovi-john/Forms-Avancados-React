9e9201bef5cd71e91a0734d0e71da9cf11b4f215
John <jovijovi2019@hotmail.com>
2023-06-06 09:46:51 -0300

    validação

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,38 @@ import { useState } from "react";
 import "./styles/global.css";
 
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 /*
  *
- * [] Validação / Transformação
+ * [x] Validação / Transformação
  * [] Field Arrays
  * [] Upload de arquivos
  * [] Composition Pattern
  *
  */
 
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty("O e-mail é obrigatório")
+    .email("Formato de e-mail inválido"),
+  password: z.string().min(6, "A senha precisa ter no mínimo 6 caracteres"),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
+// quero inferir o tipo de CreateUserFormData com base no tipo de CreateUserFormSchema
+
 export default function App() {
   const [output, setOutput] = useState("");
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
 
   function createUser(data: unknown) {
     setOutput(JSON.stringify(data, null, 2));
@@ -24,7 +43,6 @@ export default function App() {
   return (
     <main className="h-screen bg-zinc-950 flex flex-col gap-y-10 text-zinc-300 items-center justify-center">
       <form
-        action=""
         className="flex flex-col gap-y-4 w-full max-w-sm"
         onSubmit={handleSubmit(createUser)}
       >
@@ -35,6 +53,8 @@ export default function App() {
             className="border text-white shadow-sm border-zinc-600 h-10 bg-zinc-800"
             {...register("email")}
           />
+
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
 
         <div className="flex flex-col gap-y-1">
@@ -44,6 +64,7 @@ export default function App() {
             className="border text-white shadow-sm border-zinc-600 h-10 bg-zinc-800"
             {...register("password")}
           />
+          {errors.password && <span>{errors.password.message}</span>}
         </div>
 
         <button
