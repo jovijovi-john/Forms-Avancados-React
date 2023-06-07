0566c570c1a82236d53de56d57b0dda2213d3526
John <jovijovi2019@hotmail.com>
2023-06-07 19:34:06 -0300

    field arrays

--- readme.md
+++ readme.md
@@ -2,11 +2,15 @@
 
 Libs usadas:
 
-  - react-hook-form
-  - zod
-  - @hookform/resolvers (para integrar o RHF com o zod)
-  - -D tailwindcss postcss autoprefixer
+- react-hook-form
+- zod
+- @hookform/resolvers (para integrar o RHF com o zod)
+- -D tailwindcss postcss autoprefixer
 
 Comandos:
 
-  npm tailwindcss init -p
+npm tailwindcss init -p
+
+Field Arrays:
+
+Sabe quando tem um campo que pode enviar um monte de coisa? Tipo quando envia uma foto e ai pode enviar mais um monte? Então, no react hook forms se chama field arrays
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import { useState } from "react";
 import "./styles/global.css";
 
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 /*
- *
  * [x] Validação / Transformação
- * [] Field Arrays
- * [] Upload de arquivos
- * [] Composition Pattern
- *
+ * [ ] Field Arrays
+ * [ ] Upload de arquivos
+ * [ ] Composition Pattern
  */
 
 const createUserFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty("O nome é obrigatório")
+    .min(6, "O nome deve ter no mínimo 6 caracteres ")
+    .transform((name) => {
+      return name
+        .trim()
+        .split(" ")
+        .map((word) => {
+          // pegando cada palavra, alterando a primeira letra pra maiuscula e concatenando com o resto da palavra
+          return word[0].toLocaleUpperCase().concat(word.substring(1));
+        })
+        .join(" ");
+    }),
   email: z
     .string()
     .nonempty("O e-mail é obrigatório")
-    .email("Formato de e-mail inválido"),
+    .email("Formato de e-mail inválido")
+    .refine((email) => {
+      // o refine é pra adicionar validações q n tenham no zod
+      return email.endsWith("gmail.com");
+    }, "O email deve ser do gmail"),
+  // .superRefine() serve para fazer validações usando mais de um campo ( tipo senha e confirmar senha). Ou seja, fazer validações acessando todos os campos.
   password: z.string().min(6, "A senha precisa ter no mínimo 6 caracteres"),
+  techs: z
+    .array(
+      z.object({
+        title: z.string().nonempty(),
+        knowledge: z.coerce.number().min(1).max(100),
+      })
+    )
+    .min(2, "Insira pelo menos 2 tecnologias"),
 });
 
 type CreateUserFormData = z.infer<typeof createUserFormSchema>;
@@ -27,17 +52,30 @@ type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 export default function App() {
   const [output, setOutput] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
+  });
+
+  // o control é pra associar o field array ao createUserFormData
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "techs",
   });
 
   function createUser(data: unknown) {
     setOutput(JSON.stringify(data, null, 2));
     return {};
+  }
+
+  function addNewTech() {
+    append({ title: "", knowledge: 0 });
   }
 
   return (
@@ -47,24 +85,89 @@ export default function App() {
         onSubmit={handleSubmit(createUser)}
       >
         <div className="flex flex-col gap-y-1">
-          <label htmlFor="">E-mail</label>
+          <label htmlFor="name">Nome</label>
           <input
             type="text"
             className="border text-white shadow-sm border-zinc-600 h-10 bg-zinc-800"
-            {...register("email")}
+            {...register("name")}
           />
 
-          {errors.email && <span>{errors.email.message}</span>}
+          {errors.name && (
+            <span className="text-red-500 text-sm">{errors.name.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-y-1">
-          <label htmlFor="">Senha</label>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            className="border text-white shadow-sm border-zinc-700 h-10 bg-zinc-800"
+            {...register("email")}
+          />
+
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-y-1">
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
-            className="border text-white shadow-sm border-zinc-600 h-10 bg-zinc-800"
+            className="border text-white shadow-sm border-zinc-700 h-10 bg-zinc-800"
             {...register("password")}
           />
-          {errors.password && <span>{errors.password.message}</span>}
+          {errors.password && (
+            <span className="text-red-500 text-sm">
+              {errors.password.message}
+            </span>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-y-1">
+          <label
+            htmlFor="password"
+            className="flex items-center justify-between text-emerald-500 text-sm"
+          >
+            Tecnologias
+            <button onClick={addNewTech}>Adicionar</button>
+          </label>
+
+          {fields.map((field, index) => {
+            return (
+              <div key={field.id} className="flex gap-2">
+                <div className="flex flex-1 flex-col gap-y-1">
+                  <input
+                    type="text"
+                    className="border text-white shadow-sm border-zinc-700 h-10 bg-zinc-800"
+                    {...register(`techs.${index}.title`)}
+                  />
+
+                  {errors.techs?.[index]?.title && (
+                    <span className="text-red-500 text-sm">
+                      {errors.techs?.[index]?.title?.message}
+                    </span>
+                  )}
+                </div>
+
+                <div className="flex flex-col  gap-y-1">
+                  <input
+                    type="number"
+                    className="w-16 border text-white shadow-sm border-zinc-700 h-10 bg-zinc-800 "
+                    {...register(`techs.${index}.knowledge`)}
+                  />
+                  {errors.techs?.[index]?.knowledge && (
+                    <span className="text-red-500 text-sm">
+                      {errors.techs?.[index]?.knowledge?.message}
+                    </span>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+          {errors.techs && (
+            <span className="text-red-500 text-sm">{errors.techs.message}</span>
+          )}
         </div>
 
         <button
